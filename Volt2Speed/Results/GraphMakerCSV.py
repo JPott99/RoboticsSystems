36a71d11492d22331b8c6c6e0a4fbe150c3c876c
Jack Pottage <jack.pottage@gmail.com>
2021-05-07 16:07:11 +0100

    updated graph generator

--- Volt2Speed/Results/GraphMakerCSV.py
+++ Volt2Speed/Results/GraphMakerCSV.py
@@ -2,13 +2,13 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("DataVolt.csv") as file:
+with open("dataVoltAir.csv") as file:
     csvReader = csv.reader(file, delimiter=',')
     importedLeft = []
     importedRight = []
     for row in csvReader:
-        importedLeft.append(int(row[0]))
-        importedRight.append(int(row[1]))
+        importedLeft.append(abs(int(row[0])))
+        importedRight.append(abs(int(row[1])))
 
 
 x = np.linspace(0,255,255)
@@ -17,4 +17,5 @@ plt.plot(x, importedRight)
 plt.legend(["Left","Right"])
 plt.xlabel("Digital Input")
 plt.ylabel("Encoder Counts per 100ms")
-plt.savefig("DataVolt.png")
+plt.grid(1)
+plt.savefig("DataVoltAir.png")
